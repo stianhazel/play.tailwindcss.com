6412e388049bae135c2f40928b9fe2e36132737e
stainednuttz <stianhazel@gmail.com>
2022-02-19 22:56:22 +0000

    svg props

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,9 +36,9 @@ export function Header({
       <div className="flex items-center sm:hidden">
         <HeaderButton className="" onClick={() => setIsOpen(true)}>
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
           />
         </HeaderButton>
